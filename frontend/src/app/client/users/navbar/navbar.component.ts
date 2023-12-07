57accf5985b1e6776a197a778b251ec49f96dcf5
Catlin <clatcovschi@gmail.com>
2023-12-08 00:59:37 +0200

    implement add tag

--- frontend/src/app/client/users/navbar/navbar.component.ts
+++ frontend/src/app/client/users/navbar/navbar.component.ts
@@ -26,7 +26,6 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.usersService.userInfo().subscribe((res) => {
-      console.log(res)
       this.user = res;
     })
   }
